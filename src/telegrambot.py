4a2315b5bd085df268b824171861192c9cbd135c
Joel Bermudez Acosta <47182906+JoelBerus16@users.noreply.github.com>
2021-08-03 20:12:55 -0500

    Update telegrambot.py

--- src/telegrambot.py
+++ src/telegrambot.py
@@ -18,54 +18,60 @@ path_ogg = os.path.join(location_input, 'user_voice.ogg')
 path_wav = os.path.join(location_input, 'out.wav') 
 path_output = os.path.join(location_output) 
 
-if os.path.exists(location_output):
-  shutil.rmtree(location_output)
+try:
 
-@bot.message_handler(content_types=["voice","audio"])
-def get_audio_message(message):
+  if os.path.exists(location_output):
+    shutil.rmtree(location_output)
 
-  #eliminar el archivo si existe
-  if os.path.exists(path_wav):
-      os.remove(path_wav)
+  @bot.message_handler(content_types=["voice","audio"])
+  def get_audio_message(message):
 
-  r = sr.Recognizer();
-  file_info = bot.get_file(message.voice.file_id)
-  download_file = bot.download_file(file_info.file_path)
-  with open(location_input+file_name_ogg,'wb') as new_file:
-    new_file.write(download_file)
-    dest_filename = location_input+file_name_wav
-    process = subprocess.run(['ffmpeg','-i',location_input+file_name_ogg,dest_filename])
-    file = sr.AudioFile(dest_filename)
+    #eliminar el archivo si existe
+    if os.path.exists(path_wav):
+        os.remove(path_wav)
 
-    with file as source:
-      audio = r.record(source)
+    r = sr.Recognizer();
+    file_info = bot.get_file(message.voice.file_id)
+    download_file = bot.download_file(file_info.file_path)
+    with open(location_input+file_name_ogg,'wb') as new_file:
+      new_file.write(download_file)
+      dest_filename = location_input+file_name_wav
+      process = subprocess.run(['ffmpeg','-i',location_input+file_name_ogg,dest_filename])
+      file = sr.AudioFile(dest_filename)
+
+      with file as source:
+        audio = r.record(source)
+        
+        #text = r.recognize_google(audio, lenguaje ='es-ES')
+        bot.send_message(message.chat.id, 'holas, porcesando tu adio')
+        #eliminar el archivo el archivo ogg
+        if os.path.exists(path_ogg):
+            os.remove(path_ogg)
+
+  @bot.message_handler(commands=["helps"])
+  def enviar(message):
+      bot.reply_to(message, "Hola, Soy un bot de el grupo de procesos de soft.")
+
+  @bot.message_handler(commands=["start"])
+  def enviar(message):
+      bot.reply_to(message, "Hola, bienvenido")
       
-      #text = r.recognize_google(audio, lenguaje ='es-ES')
-      bot.send_message(message.chat.id, 'holas, porcesando tu adio')
-      #eliminar el archivo el archivo ogg
-      if os.path.exists(path_ogg):
-          os.remove(path_ogg)
+  @bot.message_handler(commands=["saludame"])
+  def enviar(message):
+      bot.reply_to(message, "Hola "+ message.from_user.first_name+", ¿como estas?" )
+      print(message.from_user.first_name)
 
-@bot.message_handler(commands=["helps"])
-def enviar(message):
-    bot.reply_to(message, "Hola, Soy un bot de el grupo de procesos de soft.")
+  # @bot.message_handler(commands=['foto'])
+  # def send_photo(message):
+  #     image = open("\tmp\bot.png", 'rb')
+  #     bot.send_photo(message.chat.id,image )
 
-@bot.message_handler(commands=["start"])
-def enviar(message):
-    bot.reply_to(message, "Hola, bienvenido")
-    
-@bot.message_handler(commands=["saludame"])
-def enviar(message):
-    bot.reply_to(message, "Hola "+ message.from_user.first_name+", ¿como estas?" )
-    print(message.from_user.first_name)
+  @bot.message_handler(func=lambda message: True)
+  def echo_all(message):
+    bot.reply_to(message, message.text)
 
-# @bot.message_handler(commands=['foto'])
-# def send_photo(message):
-#     image = open("\tmp\bot.png", 'rb')
-#     bot.send_photo(message.chat.id,image )
+  bot.polling()
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
-
-bot.polling()
+except telebot.apihelper.ApiException as e:
+  if e.result.status_code == 403 or e.result.status_code == 400:
+    pass   
